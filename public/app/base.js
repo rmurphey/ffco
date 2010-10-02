f322f8deaf9d71d85bf3a9723e812708b2e47edf
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-10-03 01:11:44 +0200

    like button

--- public/app/base.js
+++ public/app/base.js
@@ -1,7 +1,7 @@
 (function($){
 
 var resultsContainer = $('#results'),
-    resultsTpl = '<h2><a href="{url}">{title}</a></h2><p>{abstract}</p>',
+    resultsTpl = '<li><h2><a href="{url}">{title}</a></h2><p>{abstract}</p><p class="tools"><span class="like">like</span></p></li>',
     searchUrl = window.INTERNET ? 
       'http://query.yahooapis.com/v1/public/yql?callback=?' :
       '/data/news.json';
@@ -21,6 +21,19 @@ $('#search').submit(function(e) {
           .replace('{abstract}', r.abstract || '(No abstract)');
       }).join('')
     );
+  });
+});
+
+resultsContainer.delegate('p.tools span', 'click', function(e) {
+  var tool = $(this), url;
+
+  if (tool.hasClass('disabled')) { return; }
+  tool.addClass('disabled');
+      
+  url = tool.closest('li').find('h2 a').attr('href');
+
+  $.post('/like', { url : url }, function() {
+    alert('Like logged!');
   });
 });
 

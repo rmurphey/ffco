13f9eb959c447bde0880aab46b8978e39a71ca14
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-10-13 00:47:54 -0400

    let user choose searchers

--- public/app/views/SearchInput.js
+++ public/app/views/SearchInput.js
@@ -1,10 +1,33 @@
 require.def(function() {
   return function(searchForm) {
+    function parseForm(form) {
+      var formData = form.serializeArray(),
+          ret = {},
+          svcs = [];
+          
+      $.each(formData, function(i, field) {
+        if (field.name == 'q') {
+          ret.term = field.value;
+        } else {
+          if (field.name == 'svc') {
+            svcs.push(field.value);
+          }
+        }
+      });
+      
+      ret.svcs = svcs;
+      return ret;
+    }
+    
     searchForm.submit(function(e) {
       e.preventDefault();
-      var val = $.trim($(this).find('input').val());
-      if (!$.trim(val)) { return; }
-      $.publish('/search', [ val ]);
+      
+      var formData = parseForm($(this));
+      
+      if (!$.trim(formData.term)) { return; }
+      if (!formData.svcs.length) { return; }
+
+      $.publish('/search', [ formData.term, formData.svcs ]);
     });
   };
 });

eff6d7f53770d105090c9e616aa7b35d0ec64c4b
Rebecca Murphey <rebecca@rebeccamurphey.com>
2010-10-16 00:26:32 -0400

    removing dumb example

--- public/examples/require-def-execOnly.js
+++
@@ -1,8 +0,0 @@
-require.def(['./require-def-factory'], function(speakerFactory) {
-  var mySpeaker = speakerFactory({
-    firstName : 'Rebecca',
-    lastName : 'Murphey'
-  });
-  
-  console.log('I just do this and don\'t return anything');
-});
